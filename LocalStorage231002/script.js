6f7898493b1d2d3aa24d9bf0445cd8dbe5a2e051
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-10-02 12:14:16 +0200

    Add files via upload

---
+++ LocalStorage231002/script.js
@@ -0,0 +1,20 @@
+const szavak = [];
+
+document.getElementById("bead").onclick = () => {
+    szavak.push(document.getElementById("szo").value);
+    localStorage.setItem(lista, szavak);
+    kiir()
+}
+
+function kiir() {
+    let list = "";
+    szavak.forEach(szo => {
+        list += `<li>
+            <div>
+                <h5>${szo}</h5><p>X</p>
+            </div>
+        </li>`
+    });
+    document.getElementById("lista").innerHTML = list;
+}
+
